25cdec478e866f43eeff2a07db21e021aa1bc592
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-29 11:58:07 +0100

    Update main page to use components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import Snowfall from "react-snowfall";
 import { Hero } from "@/lib/components/Hero";
-import { InfoCard } from "@/lib/components/InfoCard";
+import InfoCard from "@/lib/components/InfoCard";
+import Features from "@/lib/components/Features";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col min-h-screen items-center justify-center bg-sky-500 font-sans dark:bg-sky-700 p-4">
+		<div className="flex flex-col min-h-screen items-center justify-center bg-sky-500 font-sans dark:bg-sky-700 p-4 overflow-x-hidden relative">
 			<Snowfall />
-			<Hero />
-			<InfoCard />
+			<div className="w-full max-w-7xl mx-auto py-12 mt-8">
+				<Hero />
+				<Features />
+				<InfoCard />
+			</div>
 		</div>
 	);
 }
