77d2306afc772cb795e527abe2ecc7a84de406fa
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-29 15:12:47 +0100

    Update feature card content

--- src/lib/components/Features.tsx
+++ src/lib/components/Features.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import FeatureCard from "./FeatureCard";
-import { ToolCase } from "lucide-react";
+import { Gift, Palette, Share, ToolCase } from "lucide-react";
 
 function Features() {
 	return (
 		<div className="w-full grid grid-cols-4 grid-rows-1 gap-4 mt-8">
 			<FeatureCard
-				title="Easy to use"
-				desc="Easy to use. Lorem ipsum dolor sit amet constructeur"
-				icon={<ToolCase />}
+				title="Daily Surprises"
+				desc="Get new custom gifts, messages or images every single day until December 24!"
+				icon={<Gift />}
 			/>
 			<FeatureCard
-				title="Easy to use"
-				desc="Easy to use. Lorem ipsum dolor sit amet constructeur"
-				icon={<ToolCase />}
+				title="Fully Customizable"
+				desc="Personalize your calendar with your own photos, colors and themes to match your style!"
+				icon={<Palette />}
 			/>
 			<FeatureCard
-				title="Easy to use"
-				desc="Easy to use. Lorem ipsum dolor sit amet constructeur"
-				icon={<ToolCase />}
+				title="Share with Friends"
+				desc="Generate a unique link to share your digital calendar with friends and family!"
+				icon={<Share />}
 			/>
 			<FeatureCard
-				title="Easy to use"
-				desc="Easy to use. Lorem ipsum dolor sit amet constructeur"
+				title="Stealing protection"
+				desc="Password-protect your advent calendar to prevent people from stealing your calendars' content!"
 				icon={<ToolCase />}
 			/>
 		</div>
