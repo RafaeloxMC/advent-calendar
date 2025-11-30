9a84c9e73dfe5703a579a9cc6d78d7a68044a7a6
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-30 16:30:49 +0100

    Add password auth for calendars

--- src/app/api/calendars/[id]/route.ts
+++ src/app/api/calendars/[id]/route.ts
@@ -1,10 +1,30 @@
 import { getCalendarByCalendarId } from "@/lib/util/db";
 import { NextRequest, NextResponse } from "next/server";
+import * as argon2 from "argon2";
 
 export async function GET(req: NextRequest) {
 	const id = req.nextUrl.pathname.split("/").pop();
+	const password = req.nextUrl.searchParams.get("password");
+
+	const calendar = await getCalendarByCalendarId(id ?? "");
+
+	if (calendar?.password_hash) {
+		if (
+			!(await argon2.verify(
+				calendar?.password_hash ?? "",
+				password ?? ""
+			))
+		) {
+			return NextResponse.json(
+				{ error: "Unauthorized" },
+				{ status: 401 }
+			);
+		}
+
+		calendar.password_hash = "";
+	}
 
 	return NextResponse.json({
-		calendar: await getCalendarByCalendarId(id ?? ""),
+		calendar: calendar,
 	});
 }
