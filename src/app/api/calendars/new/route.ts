00041808216b7e0302d7836a6d36db6c56439fa5
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-30 14:14:36 +0100

    Add calendar creation api route

--- src/app/api/calendars/new/route.ts
+++ src/app/api/calendars/new/route.ts
@@ -1,5 +1,49 @@
-import { NextResponse } from "next/server";
+import { newCalendar } from "@/lib/util/db";
+import { stackServerApp } from "@/stack/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
-	return NextResponse.json({ message: "Success" }, { status: 200 });
+export async function POST(req: NextRequest) {
+	let body;
+
+	try {
+		body = await req.json();
+	} catch {
+		return NextResponse.json({ message: "Bad request" }, { status: 400 });
+	}
+
+	if (!body || !body.year || !body.title)
+		return NextResponse.json({ message: "Bad request" }, { status: 400 });
+
+	const user = await stackServerApp.getUser();
+
+	if (!user) {
+		return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+	}
+
+	const owner_id = user.id;
+	const title = body.title;
+	const year: number = body.year;
+	let password_hash;
+
+	try {
+		const res = await newCalendar(
+			owner_id,
+			title,
+			year,
+			password_hash ?? ""
+		);
+		return NextResponse.json(
+			{ message: "Success", res: res },
+			{ status: 200 }
+		);
+	} catch (error: unknown) {
+		const err = error as { code?: string; message?: string };
+		if (err?.code === "23505" || err?.message?.includes("duplicate key")) {
+			return NextResponse.json(
+				{ message: "This calendar already exists" },
+				{ status: 409 }
+			);
+		}
+		throw error;
+	}
 }
--- src/lib/util/db.ts
+++ src/lib/util/db.ts
@@ -22,3 +22,30 @@ export async function selectAll() {
 	await conn();
 	return pool.query("SELECT * FROM neon_auth.users_sync");
 }
+
+export async function newCalendar(
+	ownerId: string,
+	title: string,
+	year: number,
+	password_hash: string
+) {
+	if (!ownerId) return;
+	if (!year) return;
+	if (!title) return;
+	await conn();
+
+	const columns = ["owner_id", "title", "year"];
+	const values = [ownerId, title, year];
+
+	if (password_hash) {
+		columns.push("password_hash");
+		values.push(password_hash);
+	}
+
+	const placeholders = values.map((_, i) => `$${i + 1}`).join(", ");
+	const query = `INSERT INTO advent_calendars (${columns.join(
+		", "
+	)}) VALUES (${placeholders})`;
+
+	return pool.query(query, values);
+}
